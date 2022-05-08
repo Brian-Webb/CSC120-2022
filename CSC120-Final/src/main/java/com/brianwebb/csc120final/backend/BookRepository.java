502138bf7d55ed67803a8747433bb46113f080ec
Brian Webb <brian.webb@goodwillaz.org>
2022-05-07 18:58:32 -0700

    Basic book files from demo video

---
+++ CSC120-Final/src/main/java/com/brianwebb/csc120final/backend/BookRepository.java
@@ -0,0 +1,8 @@
+package com.brianwebb.csc120final.backend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
