5d841b73d3c549a8fd745bfeae2a8d6d4df5dbda
Brian Webb <brian.webb@goodwillaz.org>
2022-05-07 17:33:43 -0700

    Initial Commit for the Final Project. This is the base Spring build.

---
+++ CSC120-Final/src/main/java/com/brianwebb/csc120final/Csc120FinalApplication.java
@@ -0,0 +1,13 @@
+package com.brianwebb.csc120final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csc120FinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Csc120FinalApplication.class, args);
+    }
+
+}
---
+++ CSC120-Final/src/test/java/com/brianwebb/csc120final/Csc120FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brianwebb.csc120final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csc120FinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
